4e343dad5610b3623233a0f3562d442dabe6016a
Rafael Lopez <rafael.lr@globant.com>
2022-01-21 14:32:21 -0600

    BUILD

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+
+function Movie({title, poster, release}) {
+  return (
+    <article>
+      <img src={ poster ? `https://image.tmdb.org/t/p/w500/${poster}` : '/logo512.png' } />
+      <p>{title}</p>
+      <small>{release}</small>
+    </article>
+  );
+};
+function Movies() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=66c0c0819e2b4b94de43dcc6ac9d2a21&page=1&region=MX`);
+      const json = await response.json();
+      setMovies(json.results);
+    };
+    fetchMovies();
+  }, []); //Runs only on the first render
+
+  return (
+    <section>
+      { movies ? movies.map((m)=> <Movie title={m.title} poster={m.poster_path} release={m.release_date} />)
+        : <i>Loading...</i>
+      }
+    </section>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Movies: Now playing</h1>
+      <Movies/>
+    </main>
   );
 }
 
